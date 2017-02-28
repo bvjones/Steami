db127956d80ead2735eb5dd9cbac2b91671d0786
bvjones <nanteos@hotmail.com>
2017-02-28 17:09:46 +0000

    showing persona name

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,14 @@
 'use strict';
 
 import React from 'react'
+import player from '../data/GetPlayerSummaries.jsx';
 
 export default class NavBar extends React.Component {
   render() {
     return (
       <div className="NavBar">
         <img className="SteamAvatar"/>
-        <h1 className="SteamId">SteamId Label</h1>
+        <h1 className="SteamId">{player.personaname}</h1>
       </div>
     );
   }
---
+++ src/data/GetPlayerBans.jsx
@@ -0,0 +1,14 @@
+{
+	"players": [
+		{
+			"SteamId": "76561197980367679",
+			"CommunityBanned": false,
+			"VACBanned": true,
+			"NumberOfVACBans": 1,
+			"DaysSinceLastBan": 3541,
+			"NumberOfGameBans": 0,
+			"EconomyBan": "none"
+		}
+	]
+
+}
