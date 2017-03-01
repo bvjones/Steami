913579e2870cf844428f2b35df1b36cc342a6c07
Rick Clark <rkingsleyclark@gmail.com>
2017-03-01 11:14:49 +0000

    GamesDataContainer successfully calling API

--- src/components/GamesDataContainer.jsx
+++ src/components/GamesDataContainer.jsx
@@ -4,6 +4,28 @@ import React from 'react'
 import GamesPresentationContainer from './GamesPresentationContainer.jsx'
 
 export default class GamesDataContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      games: {}
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/steam/player/76561197980367679/games")
+      .then(res => {
+        console.log("Got response:");
+        console.log(res);
+        return res.json() })
+          .then(json => {
+            console.log("Made JSON:");
+            console.log(json);
+            this.setState({ games: json })
+          });
+  }
+
+
   render() {
     return (
       <GamesPresentationContainer/>
