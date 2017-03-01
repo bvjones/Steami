fe1d7d784cc8c52d7d0da9f8b499be3bd4c9671a
Rick Clark <rkingsleyclark@gmail.com>
2017-03-01 14:04:14 +0000

    Games loading from API

--- src/components/GamesDataContainer.jsx
+++ src/components/GamesDataContainer.jsx
@@ -13,23 +13,16 @@ export default class GamesDataContainer extends React.Component {
     };
   }
 
-  componentWillMount() {
+  componentDidMount() {
     fetch("http://localhost:3000/steam/player/76561197980367679/games")
       .then(res => {
-        console.log("Got response:");
-        console.log(res);
         return res.json() })
           .then(json => {
-            console.log("Made JSON:");
-            console.log(json);
             this.setState({ games: json })
-            console.log("1");
           });
   }
 
-
   render() {
-    console.log("2");
     return (
       <GamesPresentationContainer games={this.state.games}/>
     );
--- src/components/GamesPresentationContainer.jsx
+++ src/components/GamesPresentationContainer.jsx
@@ -5,24 +5,29 @@ import gameslist from '../data/GetOwnedGames.jsx';
 import Game from './Game.jsx'
 
 export default class GamesPresentationContainer extends React.Component {
-  componentWillMount(){
-    console.log("GamesPresentationContainer mounted");
+
+  loadGames() {
+    if ( Object.keys(this.props.games).length === 0) {
+      return "Loading..."
+    } else {
+      return this.renderGamesComponents();
+    }
   }
+
+  renderGamesComponents() {
+    return this.props.games.response.games.map(
+      (game, i) => <Game key={i} {...game}/>
+    );
+  }
+
   render() {
-    console.log("rendering props");
-    console.log(this.props.games);
-    console.log(this.props.games.response);
-    console.log("2");
-
-
+    let content = this.loadGames();
     return (
       <div className="games-presentation-container">
         <h1 className="Games">Games</h1>
           <div className="GamesList">
-            <ul>{
-              this.props.games.response.games.map((game, i) => <Game key={i}
-              {...game}/>)
-              }
+            <ul>
+              { content }
             </ul>
             <img className="GameImageN"/>
             <h3 className="GameN">GameN</h3>
