a760720b2805e6cd5e39cdc105987caa805ab563
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-03-02 12:22:44 +0000

    Add two more tests

--- test/IndexPage-test.js
+++ test/IndexPage-test.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
 import IndexPage from '../src/components/IndexPage.jsx';
+import NavBar from '../src/components/NavBar.jsx';
+
 
 describe("<Index Page />", function() {
   it("contains a class called home", function() {
     expect(shallow(<IndexPage />).hasClass('home')).to.equal(true);
+  });
+});
+
+describe("<NavBar />", function() {
+  it("contains a class called home", function() {
+    const wrapper = shallow(<IndexPage />);
+    expect(wrapper.find('.NavBar')).to.have.length(0);
+    expect(wrapper.find(NavBar).render().find('.NavBar')).to.have.length(1);
+  });
+});
+
+describe("<NavBar />", function() {
+  it("contains components NavBar, StatContainer and GamesDataContainer", function() {
+    const wrapper = shallow(<IndexPage />);
+    expect(wrapper.text()).to.equal('<NavBar /><StatContainer /><GamesDataContainer />');
   });
 });
