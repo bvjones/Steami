03a0377f2adaa32b0bc5217b11f10c8adb5c8ffd
bvjones <nanteos@hotmail.com>
2017-03-03 12:52:14 +0000

    game page styling complete

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router'
 export default class Game extends React.Component {
   render() {
     return (
-      <Link to={`/games/${this.props.appid}`} className="game-flex-item m-1">
+      <Link to={`/games/${this.props.appid}`} className="game-flex-item mx-1 my-2 hover-box-shadow">
         <img className="game-image" src={`http://media.steampowered.com/steamcommunity/public/images/apps/${this.props.appid}/${this.props.img_logo_url}.jpg`}></img>
         <p className="game-name">{this.props.name}</p>
       </Link>
