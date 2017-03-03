a06f44ffa35276d25b537ceb9a34ef571dc8de09
bvjones <nanteos@hotmail.com>
2017-03-03 10:17:37 +0000

    added bootstrap and scss

--- src/app-client.js
+++ src/app-client.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRoutes from './components/AppRoutes.jsx';
+require('./static/styles/main.scss');
 
 window.onload = () => {
   ReactDOM.render(<AppRoutes/>, document.getElementById('main'));
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default class Layout extends React.Component {
   render() {
     return (
-      <div className="home">{this.props.children}</div>
+      <div className="container">{this.props.children}</div>
     );
   }
 }
